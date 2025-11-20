671f74ee1d1507d0ac2e086d2b7a09aad714e1ab
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-20 13:12:11 +0800

    Add operation-update for dispayTop

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 function computeEquation(string) {
-    // TODO: Complete the logic for computing the answer
-    console.log(string);
-    let arr = string.split(' ')
-    return '0'; // should return answer as string
+    // TODO: Logic for invalid
+    // TODO: Logic for rounding off
+    let ans = 0;
+    let arr = string.split(' ');
+    let num1 = Number(arr[0]);
+    let num2 = Number(arr[2]);
+    let operation = arr[1];
+
+    if (operation === "+") ans = num1 + num2;
+    else if (operation === "-") ans = num1 - num2;
+    else if (operation === "%") ans = num1 / num2;
+    else if (operation === "×") ans = num1 * num2;
+
+    console.log(`Operation performed: ${string} = ${ans}`);
+
+    return String(ans);
 }
 
 const displayTop = document.querySelector(".display-top");
 const displayBottom = document.querySelector(".display-bottom");
 const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
 const numberBtn = document.querySelectorAll(".number");
 const operationBtn = document.querySelectorAll(".operation");
 // TODO: Implement point button
-// TODO: Add backspace button
 const pointBtn = document.querySelector(".point");
 const equalBtn = document.querySelector(".equal");
 
@@ -22,6 +34,12 @@ clearBtn.addEventListener('click', () => {
     displayBottom.textContent = '';
 });
 
+deleteBtn.addEventListener('click', () => {
+    let arr = displayBottom.textContent.split('');
+    arr.pop();
+    displayBottom.textContent = arr.join('');;
+});
+
 numberBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
         displayBottom.textContent = `${displayBottom.textContent}` + btn.textContent; 
@@ -30,17 +48,21 @@ numberBtn.forEach((btn) => {
 
 operationBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
-        if (displayBottom.textContent === '') {
-            // TODO: update the operation on displayTop with the clicked operation
-            return;
+        if (displayBottom.textContent || displayTop.textContent) {
+            if (!displayTop.textContent) {
+                displayTop.textContent = `${displayBottom.textContent} ${btn.textContent}`;
+                displayBottom.textContent = '';
+            } else if (!displayBottom.textContent) {
+                let arr = displayTop.textContent.split('');
+                arr.pop();
+                arr.push(btn.textContent);;
+                displayTop.textContent = arr.join('');
+            } else if (displayTop.textContent && displayBottom.textContent) {
+                let equation = `${displayTop.textContent} ${displayBottom.textContent}`;
+                displayTop.textContent = `${computeEquation(equation)} ${btn.textContent}`;
+                displayBottom.textContent = '';
+            }
         }
-        if (operations.some(operation => displayTop.textContent.includes(operation))) {
-            let equation = `${displayTop.textContent} ${displayBottom.textContent}`;
-            displayTop.textContent = `${computeEquation(equation)} ${btn.textContent}`;
-        } else {
-            displayTop.textContent = `${displayBottom.textContent} ${btn.textContent}`;
-        }
-        displayBottom.textContent = '';
     });
 });
 
