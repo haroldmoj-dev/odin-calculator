2aa4b04aee507cbe0b6d5a325a62e2ccb7d29fcc
haroldmoj-dev <haroldmojica.dev@gmail.com>
2025-11-20 14:55:41 +0800

    Add rounding and removal of trailing zeroes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,40 @@
 function computeEquation(string) {
     // TODO: Logic for invalid
-    // TODO: Logic for rounding off
-    // TODO: Overflow of elements css in result
-        // if includes 'e', if includes '.'
     let ans = 0;
     let arr = string.split(' ');
     let num1 = Number(arr[0]);
     let num2 = Number(arr[2]);
     let operation = arr[1];
 
-    if (operation === "+") ans = num1 + num2;
-    else if (operation === "-") ans = num1 - num2;
-    else if (operation === "%") ans = num1 / num2;
-    else if (operation === "×") ans = num1 * num2;
+    // perform operation and round to 6 decimal places
+    if (operation === "+") ans = (num1 + num2).toFixed(6);
+    else if (operation === "-") ans = (num1 - num2).toFixed(6);
+    else if (operation === "%") ans = (num1 / num2).toFixed(6);
+    else if (operation === "×") ans = (num1 * num2).toFixed(6);
 
+    // remove trailing zeroes at the end of decimal
+    if (ans.includes('.') && !ans.includes('e')) {
+        ansArr = ans.split('');
+        while (ansArr[ansArr.length - 1] === '0') ansArr.pop();
+        if (ansArr[ansArr.length - 1] === '.') ansArr.pop();
+        ans = ansArr.join('');
+    }
+
+    // TODO: Overflow of elements css in result
+    if (ans.length > 12) {
+        if (!ans.includes('e')) {
+            if (ans.includes('.')) {
+                
+            } else {
+
+            }
+        } else {
+
+        }
+    }
     console.log(`Operation performed: ${string} = ${ans}`);
 
-    return String(ans);
+    return ans;
 }
 
 const displayTop = document.querySelector(".display-top");
@@ -58,7 +76,10 @@ pointBtn.addEventListener('click', () => {
 
 operationBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
+        // does nothing if display is empty
         if (displayBottom.textContent || displayTop.textContent) {
+
+            // remove leading zeroes and copy displayBottom to displayTop
             if (!displayTop.textContent) {
                 let arr = displayBottom.textContent.split('');
                 while (arr[0] === '0') {
@@ -69,11 +90,15 @@ operationBtn.forEach((btn) => {
                 }
                 displayTop.textContent = `${arr.join('')} ${btn.textContent}`;
                 displayBottom.textContent = '';
+
+            // update displayTop's operation
             } else if (!displayBottom.textContent) {
                 let arr = displayTop.textContent.split('');
                 arr.pop();
                 arr.push(btn.textContent);;
                 displayTop.textContent = arr.join('');
+
+            // compute and update displayTop
             } else if (displayTop.textContent && displayBottom.textContent) {
                 let equation = `${displayTop.textContent} ${displayBottom.textContent}`;
                 displayTop.textContent = `${computeEquation(equation)} ${btn.textContent}`;
